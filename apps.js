9dabac28b8597a9e0263f888790395800fd76042
Alejandro <michi@gmail.com>
2025-04-24 12:36:52 -0400

    inicio

---
+++ apps.js
@@ -0,0 +1,24 @@
+document.write("<h1> hola javaScript")
+//comentarios de una line
+/* comentarios de bloque */
+
+let nombre = "Carlos"
+
+console.log(nombre)
+
+//Array
+
+let datos=["Ana", "Pablo", "Carlos"]
+
+let edades=[23, 17, 44, 15]
+
+let objetoDatos={
+    nombre:"Ana",
+    edad:25,
+    cuenta:"Alumno",
+    notas:[5.5, 6.8, 5.9]
+}
+
+console.log(datos)
+console.log(edades)
+console.log(objetoDatos)
