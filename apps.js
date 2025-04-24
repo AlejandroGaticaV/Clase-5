52b24dd2bc1e80488616af6a1cf88f7381109166
Alejandro <michi@gmail.com>
2025-04-24 13:34:41 -0400

    Impares

--- apps.js
+++ apps.js
@@ -8,15 +8,15 @@ console.log(nombre)
 
 //Array
 
-let datos=["Ana", "Pablo", "Carlos"]
+let datos = ["Ana", "Pablo", "Carlos"]
 
-let edades=[23, 17, 44, 15]
+let edades = [23, 17, 44, 15]
 
-let objetoDatos={
-    nombre:"Ana",
-    edad:25,
-    cuenta:"Alumno",
-    notas:[5.5, 6.8, 5.9]
+let objetoDatos = {
+    nombre: "Ana",
+    edad: 25,
+    cuenta: "Alumno",
+    notas: [5.5, 6.8, 5.9]
 }
 
 console.log(datos)
@@ -34,24 +34,24 @@ console.log(objetoDatos)
 ===
 */
 
-let uno=1
-let unoString="1"
+let uno = 1
+let unoString = "1"
 
-console.log(uno==unoString)
-console.log(uno===unoString)
+console.log(uno == unoString)
+console.log(uno === unoString)
 
 //estructuras de control
 
-if (uno==unoString)
+if (uno == unoString)
     console.log("Son Iguales ?")
-if (uno===unoString){
+if (uno === unoString) {
     console.log("son Iguales")
 }
-else{
+else {
     console.log("No son Iguales")
 }
 
-let tipoAlumno="diurno"
+let tipoAlumno = "diurno"
 
 switch (tipoAlumno) {
     case "diurno":
@@ -69,3 +69,70 @@ switch (tipoAlumno) {
 
 /* mostrar los primeros 10 numeros positivos */
 
+let numero = 1;
+
+while (numero <= 10) {
+    console.log(numero)
+    numero = numero + 1
+
+}
+
+//for
+
+let nombres = ["Ana", "Pablo", "Carlos"]
+
+for (let i = 0; i < nombres.length; i++) {
+    console.log(nombres[i])
+}
+
+//funciones
+
+function mensaje() {
+    console.log("Saludos")
+}
+
+mensaje()
+
+//Nota: if( numero % 2 == 0)
+
+/*
+Muestre la cantidad de numeros pares que 
+existen dentro de un arreglo
+use una funcion que retorne
+si el numero es par o inpar o no.
+*/
+
+let numeros = [45, 33, 14, 23, 20, 4, 7, 12, 68, 3];
+
+let contP = 0
+
+function es_par(num){
+    if(num %2==0)
+        return true
+    return false
+}
+
+for(let i=0;i<numeros.length;i++){
+    if(es_par(numeros[i]))
+        contP++
+}
+
+console.log("Los pares son ", contP)
+
+// Modifique el ejercicio para mostrar la
+// cantidad de impares
+
+let contI = 0
+
+function es_impar(num){
+    if(num %2 !==0)
+        return true
+    return false
+}
+
+for(let i=0;i<numeros.length;i++){
+    if(es_impar(numeros[i]))
+        contI++
+}
+
+console.log("Los numeros impares ", contI)
